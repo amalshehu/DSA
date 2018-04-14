55a307d3bacc6da41caef67001e46afb14743b0f
amalshehu <amalshehu@gmail.com>
2018-04-14 16:52:47 +0530

    Recursion

---
+++ recursion.js
@@ -0,0 +1,24 @@
+// Recursion in computer science is a method where the solution to
+// a problem depends on solutions to smaller instances of
+// the same problem (as opposed to iteration).
+// The approach can be applied to many types of problems,
+// and recursion is one of the central ideas of computer science.(src: wiki)
+
+// Basically, A function calling itself.
+
+// You should define a base case and termination condition.
+// Your function should move towards the base condition.
+// Else you may get a stack overflow error.
+
+// Fibonacci example using recursion.
+function fibonacci(n) {
+  return n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+// Factorial
+function factorial(n) {
+  return n < 2 ? 1 : n * factorial(n - 1)
+}
+
+// Date difference
+// Go to: https://github.com/amalshehu/date-difference/blob/master/date-difference.js
